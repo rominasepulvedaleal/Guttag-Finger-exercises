e506c40e748206a69620a1c86b0bf1b3540b193f
rominasepulvedaleal <89989186+rominasepulvedaleal@users.noreply.github.com>
2021-10-11 17:36:58 +0300

    Add files via upload

---
+++ Finger_exercise_2.1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 16:23:00 2021
+
+@author: rominasepulveda
+"""
+
+# Guttag-Finger-exercises
+# 2.1: Write a program that examines three variables— x , y , and z — 
+# and prints the largest odd number among them. 
+# If none of them are odd, it #should print a message to that effect.
+
+x=int(input('Enter x: '))
+y=int(input('Enter y: '))
+z=int(input('Enter z: '))
+
+if x>y>z and x%2==1:
+    print(str(x),' is the largest odd number')
+elif y>z and y%2==1:
+    print(str(y),' is the largest odd number')
+elif z%2==1:
+    print(str(z),' is the largest odd number')
+else:
+    print('none of them is odd')
---
+++ Finger_exercise_2.2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 17:17:38 2021
+
+@author: rominasepulveda
+"""
+
+#you haven't worked on it yet
